bfd412b6fec26ff37e385ad7d8ec915bf6c861cd
GYoung <ytok1108@aol.com>
2025-07-26 19:18:30 +0900

    std Vector add ++ bench

--- benches/benchmarks/arc_vec_sort_test.rs
+++ benches/benchmarks/arc_vec_sort_test.rs
@@ -1,6 +1,6 @@
 use arc_vec::alloc::arc_vec::ArcVec;
-use criterion::{Criterion, criterion_group, criterion_main};
-use rand::{rng, seq::SliceRandom};
+use criterion::{criterion_group, criterion_main, Criterion};
+use rand::{rng, seq::SliceRandom, Rng};
 
 fn generate_data(size: usize) -> ArcVec<i32> {
     let mut vec: Vec<i32> = (0..size as i32).collect();
@@ -22,6 +22,14 @@ pub fn bench_sort(c: &mut Criterion) {
         b.iter(|| {
             let arcvec = generate_data(size);
             arcvec.parallel_sort();
+        })
+    });
+
+    c.bench_function("std_vec::sort", |b| {
+        b.iter(|| {
+            let mut rng = rand::rng();
+            let mut std_vec: Vec<i32> = (0..size).map(|_| rng.random_range(0..1_000_000)).collect();
+            std_vec.sort();
         })
     });
 }
